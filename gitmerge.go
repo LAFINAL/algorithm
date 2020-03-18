396f99ad41afbdf9c1b540561f66788a45f313d2
6826587 <wonki_kim@hyundai.com>
2020-03-18 12:45:01 +0900

    Add test code in master branch

--- gitmerge.go
+++ gitmerge.go
@@ -5,4 +5,5 @@ import "fmt"
 func main(){
 	fmt.Println("Add test code in master branch first")
 	fmt.Println("Add test code in develop branch later")
+	fmt.Println("Add test code in master branch later")
 }
