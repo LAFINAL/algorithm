4a6b9261d1526b62ed1f86adc4665e4c350a10a0
4orty <kwk5178@gmail.com>
2021-05-04 15:08:48 +0900

    add leetcode 1769 solution

--- leetcode/1769.Minimum_Number_of_Operations_to_Move_All_Balls_to_Each_Box.go
+++ leetcode/1769.Minimum_Number_of_Operations_to_Move_All_Balls_to_Each_Box.go
@@ -1,1 +1,27 @@
 package leetcode
+
+import "fmt"
+
+func minOperations(boxes string) [] int {
+	nonEmpty := make([]int, 0, 0)
+	for i:=0; i<len(boxes); i++ {
+		if boxes[i] == '1' {
+			nonEmpty = append(nonEmpty, i)
+		}
+	}
+	fmt.Println("nonEmpty array: ", nonEmpty)
+
+	answer := make([]int, len(boxes), len(boxes))
+	for i:=0; i<len(boxes); i++ {
+		operation := 0
+		for j:=0; j<len(nonEmpty); j++ {
+			distance := nonEmpty[j] - i
+			if distance < 0 {
+				distance = distance * -1
+			}
+			operation = operation + distance
+		}
+		answer[i] = operation
+	}
+	return answer
+}
--- leetcode/1769.Minimum_Number_of_Operations_to_Move_All_Balls_to_Each_Box_test.go
+++ leetcode/1769.Minimum_Number_of_Operations_to_Move_All_Balls_to_Each_Box_test.go
@@ -1,1 +1,27 @@
 package leetcode
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_minOperations(t *testing.T) {
+	type args struct {
+		boxes string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{"110", args{"110"}, []int{1,1,3}},
+		{"001011", args{"001011"}, []int{11,8,5,4,3,4}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := minOperations(tt.args.boxes); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("minOperations() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
